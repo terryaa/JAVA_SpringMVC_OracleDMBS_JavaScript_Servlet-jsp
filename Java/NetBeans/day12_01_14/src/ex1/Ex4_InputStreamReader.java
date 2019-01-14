b8637a30ac5e58ae709254113ae110e140326b22
younghoon <terryaaaa@gmail.com>
2019-01-14 12:43:57 +0900

    19_01_14
    12:43pm

--- Java/NetBeans/day12_01_14/src/ex1/Ex4_InputStreamReader.java
+++ Java/NetBeans/day12_01_14/src/ex1/Ex4_InputStreamReader.java
@@ -5,10 +5,62 @@
  */
 package ex1;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author younghoonkim
  */
 public class Ex4_InputStreamReader {
+    
+    public static void main(String[] args) {
+        BufferedReader br=null;
+           String path="/Users/younghoonkim/Documents/KOSTA_Git/KOSTA_MAC/Java/NetBeans/day"
+                + "12_01_14/src/ex1/memo.txt";
+           String path2="https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EB%9D%BC%EC%9D%B4%ED%84%B0";
+        try {
+            br=new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+            
+            //There's need to read ByteStream in form of String stream.
+            //In this case we use BridgeStream to change bytestream to stringstream.
+            //Practice : use  Scanner API and create class Ex5_ScannerDemo, which
+            //has same functions as this class.
+            String str=null;
+            while((str=br.readLine())!=null){
+                System.out.println(str);
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Ex4_InputStreamReader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Ex4_InputStreamReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        URL url;
+        try {
+            url = new URL(path2);
+            br=new BufferedReader(new InputStreamReader(url.openStream(),"euc-kr"));
+            
+            String str=null;
+            while((str=br.readLine())!=null){
+                System.out.println(str);
+            }
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Ex4_InputStreamReader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Ex4_InputStreamReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+                
+    
+    }
+ 
     
 }

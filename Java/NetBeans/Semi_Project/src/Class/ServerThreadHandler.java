095c6b7375b71c7b9a0333dff56cad44ef730bcb
younghoonkim <terryaaaa@naver.com>
2019-01-29 23:50:16 +0900

    01_29
    11:50pm

--- Java/NetBeans/Semi_Project/src/Class/ServerThreadHandler.java
+++ Java/NetBeans/Semi_Project/src/Class/ServerThreadHandler.java
@@ -25,10 +25,37 @@ public class ServerThreadHandler implements ServerThreadHandlerInter{
     
     private final String path="/Users/younghoonkim/Documents/KOSTA_Git/KOSTA_MAC"
                     + "/Java/NetBeans/Semi_Project/src/Data/";
-    
+    JSONParser parser = null; // 불러오기?
+    JSONObject memberInfo = null; // 값에 대한 객체 생성
+    JSONObject members=null;
+//     JSONParser parser = new JSONParser(); // 불러오기?
+//    JSONObject memberInfo = new JSONObject(); // 값에 대한 객체 생성
     
      public String idcheck(String msg){
-         StringTokenizer st=new StringTokenizer(msg, ":");
+         StringTokenizer st=new StringTokenizer(msg, "^");
+         String identifier=st.nextToken();
+         String userId=st.nextToken();
+         StringBuffer sb=new StringBuffer();
+        try {
+            members = (JSONObject) parser.parse(new FileReader(path+"member.json"));
+            memberInfo= (JSONObject) members.get(userId);
+            if(memberInfo==null) {
+                return "false";
+            }
+            else{//나중에 check으로통일가능하면통일
+                if(identifier.equals("search") || identifier.equals("check")){
+                    sb.append("true");
+                }
+                
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ServerThreadHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(ServerThreadHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
+         
          if(!containsId(st.nextToken())){
              return "false";
          }
@@ -37,6 +64,8 @@ public class ServerThreadHandler implements ServerThreadHandlerInter{
          }
          else
              
+         
+                      
          
          
      }
